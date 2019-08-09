f92e9d6d9013b41c257fcb831ba1d11804f0cfd7
Александр Владимирович Ширшов <kkeanor@gmail.com>
2019-08-10 00:11:26 +0300

    Рабочий вариант web сервера

--- src/webserver.cpp
+++ src/webserver.cpp
@@ -44,13 +44,23 @@ class WebServer : public IWebServer {
 
         void favicon(void) {
             server->setContentLength(resources_favicon_png_len);
-            server->send(200, "image/png", (char*)resources_favicon_png);
+            server->send_P(
+                200, 
+                "image/png", 
+                (char*)resources_favicon_png,
+                resources_favicon_png_len
+            );
         }
 
         void stylesheet(void) {
             server->setContentLength(resources_skeleton_min_css_tar_gz_len);
             server->sendHeader("Content-Encoding", "gzip");
-            server->send(200, "text/css", (char*)resources_skeleton_min_css_tar_gz);
+            server->send_P(
+                200, 
+                "text/css", 
+                (char*)resources_skeleton_min_css_tar_gz, 
+                resources_skeleton_min_css_tar_gz_len
+            );
         }
 };
 
